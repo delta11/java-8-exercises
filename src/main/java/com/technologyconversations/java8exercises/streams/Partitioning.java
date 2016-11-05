6ece14d199f0229492de1263fabd7547fe0245d5
Thomas van Putten <thomasvanputten@hotmail.com>
2016-11-05 15:14:38 +0100

    Removed answers for use in kata

--- src/main/java/com/technologyconversations/java8exercises/streams/Partitioning.java
+++ src/main/java/com/technologyconversations/java8exercises/streams/Partitioning.java
@@ -1,26 +1,17 @@
 package com.technologyconversations.java8exercises.streams;
 
-import java.util.*;
-import static java.util.stream.Collectors.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Partitioning {
 
     private Partitioning() {
     }
 
-    public static Map<Boolean, List<Person>> partitionAdults7(List<Person> people) {
-        Map<Boolean, List<Person>> map = new HashMap<>();
-        map.put(true, new ArrayList<>());
-        map.put(false, new ArrayList<>());
-        for (Person person : people) {
-            map.get(person.getAge() >= 18).add(person);
-        }
-        return map;
-    }
-
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
-        return people.stream() // Convert collection to Stream
-                .collect(partitioningBy(p -> p.getAge() >= 18)); // Partition stream of people into adults (age => 18) and kids
+        return null;
     }
 
 }
